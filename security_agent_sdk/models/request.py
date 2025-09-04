030c65c33fdcd004c1b473f94a19d380b844c168
Roy Mustang <FullmetalHodler@logarithm.fi>
2025-09-04 12:10:47 +0000

    update request

--- security_agent_sdk/models/request.py
+++ security_agent_sdk/models/request.py
@@ -1,20 +1,5 @@
-from typing import List, Optional
-
-from pydantic import BaseModel, HttpUrl
-from yield_analysis_sdk.type import Chain
-from yield_analysis_sdk.validators import ChainMixin
-
-from ..validation import AddressValidatorMixin
+from yield_analysis_sdk.type import RegistrationRequest
 
 
-class Contract(AddressValidatorMixin, ChainMixin, BaseModel):
-    address: str
-    chain: Chain
-
-
-class RegistrationRequest(BaseModel):
-    """Registration request with vault contract and additional contracts list."""
-
-    vault: Contract
-    contracts: Optional[List[Contract]] = None
-    github_repo_url: Optional[HttpUrl] = None
+class AuditRequest(RegistrationRequest):
+    pass
